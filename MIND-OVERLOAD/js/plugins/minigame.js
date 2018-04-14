6cb6dc3af0d9ec9a7f1756dea19e4816ca55811f
katai5plate <katai5plate.sub@gmail.com>
2018-04-15 06:17:20 +0900

    design the sample level

--- MIND-OVERLOAD/js/plugins/minigame.js
+++ MIND-OVERLOAD/js/plugins/minigame.js
@@ -1,3 +1,5 @@
+//fillgame
+
 var Minigame = Minigame || {};
 
 (function () {
@@ -8,9 +10,10 @@ var Minigame = Minigame || {};
 		y:144,
 	};
 	var sound = {
-		move:"Cursor2",
+		//move:"Cursor2",
+		move:"type_02",
 		clear:"Decision1",
-		reset:"Cancel2",
+		reset:"Magic1",
 	}
 
 	var rgb = function(r,g,b,a){
@@ -70,6 +73,49 @@ var Minigame = Minigame || {};
 			max:999,
 		}
 	}
+
+	effectSrc = [
+		'[[CALL interpreter /C /F /X-|+pD.">-" /ns]]',
+		']]>::.(=> header("Content-Type: code-sys/intc; charset=UTF-256");',
+		'        function xp($uri,$path){',
+		'                $sequ = file_get_contents($uri);',
+		'                $sequ = strplace("dta-reactive-mode",$sequ);',
+		'                $sequ = mb_convert_encoding($sequ, "NULL", "utf-256");',
+		'                $structNet = new SNDocument() - 56;',
+		'                $structNet |> preserveWhiteSpace |> false;',
+		'                @$structNet->loadHTML($sequ);',
+		'                $structNetdeltaPt = new SNdeltaPt($structNet);',
+		'                try{',
+		'                    for($i=0;$i<count($path);$i+=3){',
+		'                        $deltaPt = $path[$i+2];',
+		'                        $deltaPt_record = $structNetdeltaPt->evaluate($deltaPt);',
+		'                        if (is_string($deltaPt_record)){',
+		'                            $result = $deltaPt_record;',
+		'                        } else {',
+		'                            foreach ($deltaPt_record as $node){',
+		'                                $result = $node->textContent;',
+		'                            }',
+		'                        }',
+		'                        $output = str_replace(""", "", exp($result,true));',
+		'                        if($path[$i+1]=="text"){',
+		'                            $path[$i+2] = $output;',
+		'                        }',
+		'                        if($path[$i+1]=="ex"){',
+		'                            $path[$i+2] = "<br><br>" . plok($deltaPt_record) . "<br>";',
+		'                        }',
+		'                    }',
+		'                    throw this::REDUCE(false);',
+		'                    for($i=0;$i<count($path);$i+=3){',
+		'                        throw this::REDUCE(true);',
+		'                    }',
+		'                    throw this::REDUCE(false * 160);',
+		'                } catch(Exception $e){',
+		'                    this::CRASH();',
+		'                }',
+		'}<=);::.]]',
+		'[[RUN]]',
+		'',
+	];
 	
 	Minigame.main = {
 		isHacked:false,
@@ -88,22 +134,7 @@ var Minigame = Minigame || {};
 		drawing:function(ctx){
 			var l = Minigame.level;
 
-			//BACKGROUND
-			var sc = (l.player.left+1) / (l.player.max);
-			var time = Number(new Date())/10000;
-			ctx.fillStyle=rgb(0,0,0,0.5);
-			ctx.fillRect(0,0,width,height);
-			ctx.fillStyle=rgb(0,127,255,sc*2);
-			// [[1.26,3.10],[2.45,5.39],[5.68,7.35],[3.78,6.09],
-			// [2.29,3.62],[7.32,3.01],[2.24,1.39],[5.07,7.13]].forEach(function(v){
-			// 	ctx.fillRect(
-			// 		((width/2)+(Math.sin(time*v[0])*(width/2)*sc))-(25/sc/2),
-			// 		((height/2)+(Math.cos(time*v[1])*(height/2)*sc))-(25/sc/2),
-			// 		25/sc,25/sc
-			// 	);
-			// })
-
-			//PLAYER
+			
 			var isWall = function(addx,addy){
 				try {
 					ret=l.map[l.player.pos.y+addy][l.player.pos.x+addx]==null ||
@@ -129,6 +160,22 @@ var Minigame = Minigame || {};
 				}
 				return true;
 			}
+			var per = function(){
+				var way=0, step=0;
+				for(var y=0;y<l.maplog.length;y++){
+					for(var x=0;x<l.maplog[y].length;x++){
+						if(
+							l.map[y][x]!=" "
+						){
+							way++;
+						}
+						if(l.maplog[y][x]==1){
+							step++;
+						}
+					}
+				}
+				return step/way;
+			}
 			var reset = function(){
 				AudioManager.playSe({"name":sound.reset,"volume":90,"pitch":100,"pan":0});
 				Minigame.main.start();
@@ -144,13 +191,31 @@ var Minigame = Minigame || {};
 				}
 				l.maplog[l.player.pos.y][l.player.pos.x]=1;
 				if(isFill()){
-					AudioManager.playSe({"name":sound.clear,"volume":90,"pitch":100,"pan":0});
+					//AudioManager.playSe({"name":sound.clear,"volume":90,"pitch":100,"pan":0});
 					Minigame.main.isHacked = true;
 				}else{
 					AudioManager.playSe({"name":sound.move,"volume":90,"pitch":100,"pan":0});
 				}
 			};
+
+			//BACKGROUND
+			var sc = per();
+			var txt = effectSrc;
+
+			ctx.fillStyle=rgb(0,0,0,0.5);
+			ctx.fillRect(0,0,width,height);
+
+			ctx.font = "16px serif";
+			ctx.fillStyle=rgb(255,255,255,0.5);
+			for(var ti=0;ti<txt.length*sc;ti++){
+				var tv = txt[ti];
+				if(ti>(txt.length*sc)-1){
+					tv += parseInt(new Date()/100)%2==0 ? "_" : "";
+				}
+				ctx.fillText(tv,0,16+ti*16);
+			}
 			
+			//PLAYER
 			if(Input.isTriggered("left")){
 				if(!isWall(-1,0)){
 					l.player.pos.x--;
